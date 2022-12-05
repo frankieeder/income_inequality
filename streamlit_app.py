278d6152899cfc3b312c759fa9d0c3694e36cab9
Frankie Eder <frankaeder@gmail.com>
2022-12-04 19:46:01 -0800

    Working single page in multipage approach.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,50 @@
 import streamlit as st
+import plotly.express as px
+from data import CountyGeoJSON
+from data import IRSIncomeByCounty
 
-st.set_page_config(
-    page_title="Hello",
-    page_icon="👋",
-)
 
-st.write("# Welcome to Streamlit! 👋")
+@st.cache
+def get_irs_income_by_county():
+    return IRSIncomeByCounty().process()
 
-st.sidebar.success("Select a demo above.")
 
-st.markdown(
-    """
-    Streamlit is an open-source app framework built specifically for
-    Machine Learning and Data Science projects.
-    **👈 Select a demo from the sidebar** to see some examples
-    of what Streamlit can do!
-    ### Want to learn more?
-    - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in our [community
-        forums](https://discuss.streamlit.io)
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-"""
-)
+@st.cache
+def get_county_geo_json():
+    return CountyGeoJSON().process()
+
+
+def county_map():
+
+    county_sums = get_irs_income_by_county()
+    county_boundaries = get_county_geo_json()
+    metric = st.selectbox(
+        label="Metric",
+        options=list(IRSIncomeByCounty.METRIC_NAMES.keys()),
+        format_func=lambda o: IRSIncomeByCounty.METRIC_NAMES[o],
+    )
+    fig = px.choropleth(
+        county_sums,
+        geojson=county_boundaries,
+        locations=county_sums.index,
+        color=metric,
+        color_continuous_scale="Viridis",
+        featureidkey='id',
+        # range_color=(0, 12),
+        scope="usa",
+        labels={metric: IRSIncomeByCounty.METRIC_NAMES[metric]},
+        height=500
+    )
+    st.plotly_chart(fig, use_container_width=True)
+
+
+st.set_page_config(page_title="By County", page_icon="📈", layout='wide')
+
+page_names_to_funcs = {
+    "County Map": county_map,
+    #"Page 2": page2,
+    #"Page 3": page3,
+}
+
+selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+page_names_to_funcs[selected_page]()
