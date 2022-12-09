07a6150bd613b36b677cd48d1ded4b970c4f26d3
Frankie Eder <frankaeder@gmail.com>
2022-12-08 18:47:22 -0800

    Added JSON Source.

---
+++ data/json_source.py
@@ -0,0 +1,13 @@
+from urllib.request import urlopen
+import json
+
+from .data_source import DataSource
+
+
+class JSONDataSource(DataSource):
+    URL = ''
+
+    def source(self):
+        with urlopen(self.URL) as response:
+            as_dict = json.load(response)
+        return as_dict
